832e875998f254b7df3b9d594ec6a9139f3dfda9
NaheunSong <snhssn00169@naver.com>
2021-04-10 21:39:12 +0900

    2주차과제

---
+++ test.py
@@ -0,0 +1,22 @@
+print(5+7)
+print(5-7)
+print(5*7)
+print(5/7)
+
+print(5//7)
+print(2**1)
+print(2**2)
+
+print("안녕하세요")
+print('안녕하세요')
+print('"안녕하세요"라고 말했습니다"')
+print("안녕하세요"+"반갑습니다")
+
+array=[273,"문자열",True]
+print(array)
+print(array[0])
+array[0]=274
+print(array)
+
+포켓몬={"이름":"피카츄","타입":"전기","공격력":55}
+print(포켓몬["이름"])
