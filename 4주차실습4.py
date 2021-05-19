bd8bb9367a5e8cde448a5465869e91dd9d95cb03
NaheunSong <snhssn00169@naver.com>
2021-05-19 18:29:59 +0900

    commit

---
+++ 4주차실습4.py
@@ -0,0 +1,22 @@
+pokemons = ['피카츄','파이리','꼬북이']
+for pokemon in pokemons:
+    print(pokemon)
+
+for letter in '잠만보':
+    print(letter)
+
+for num in range(3):
+    print(f"피카츄 {num}마리")
+
+pokemons = ['피카츄','파이리','꼬북이']
+for num in range(3):
+    pokemons[num] = "이브이"
+print(pokemons)
+print(num)
+
+num = 0
+while num < 3:
+    print(f'이상해씨가 {num}일째 씨 뿌리는 중')
+    num = num + 1
+print("다 심었다!")
+
---
+++ 4주차어쩌구.py
@@ -0,0 +1,24 @@
+print(True)
+print(False)
+
+print(1<2)
+print(2<1)
+
+print(1<=2)
+print(2<=1)
+
+print(1==1)
+print(1!=1)
+
+win=True
+if win:
+    print("게임에서 승리했습니다.")
+
+print("You're Win! True or False?")
+win = bool(input("if you're lose, Just press enterKey "))
+if win:
+    print("게임에서 승리했습니다.")
+else:
+    print("게임에서 패배했습니다.")
+
+
